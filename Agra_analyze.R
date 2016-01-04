2fd9602eca430ce532be221e4c73b741553adc3c
bbarres <benoit.barres@gmail.com>
2016-01-04 18:12:04 +0100

    adding MLG rarefaction index

--- Agra_analyze.R
+++ Agra_analyze.R
@@ -80,12 +80,12 @@ def.par <- par(no.readonly = TRUE)
 layout(matrix(c(1,2,3,4,5),5,1,byrow=TRUE),
        widths=c(2,2,2,2,2),heights=c(1,1,1,1,1))
 par(mar = c(2,2,1,1))
-barplot(rowSums(AgrOcc),axes=TRUE,axisnames=FALSE)
-plot(specnumber(AgrOcc)/rowSums(AgrOcc),type="b",main="G/N")
-plot(diversity(AgrOcc,index="shannon"),type="b",main="Shannon")
-plot(diversity(AgrOcc,index="simpson"),type="b",main="Simpson")
-plot(diversity(AgrOcc,index="invsimpson"),type="b",main="Invert Simpson")
-
+barplot(rowSums(AgrOcc),axes=TRUE,axisnames=FALSE,space=0,xlim=c(0.5,13.5))
+plot(specnumber(AgrOcc)/rowSums(AgrOcc),type="b",main="G/N",xlim=c(1,14))
+plot(diversity(AgrOcc,index="shannon"),type="b",main="Shannon",xlim=c(1,14))
+plot(diversity(AgrOcc,index="simpson"),type="b",main="Simpson",xlim=c(1,14))
+#plot(diversity(AgrOcc,index="invsimpson"),type="b",main="Invert Simpson")
+plot(rarefy(AgrOcc,min(rowSums(AgrOcc))),type="b",main="MLG richness",xlim=c(1,14))
 
 par(def.par)
 
