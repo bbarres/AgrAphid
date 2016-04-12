e2085fa9776c8e35e08745951b899a276d15fa83
bbarres <benoit.barres@gmail.com>
2016-04-13 01:04:19 +0200

    cosmetic changes

--- Agra_host_div.R
+++ Agra_host_div.R
@@ -30,6 +30,7 @@ Host_MLG_richness<-rarefy(hostocc,min(rowSums(hostocc)))
 Host_simpson_div<-diversity(hostocc,index="simpson")
 Host_pielou_even<-diversity(hostocc)/log(specnumber(hostocc))
 
+
 ###############################################################################
 #Genetic Diversity indices by host on the complete dataset
 ###############################################################################
@@ -46,7 +47,7 @@ COMPDIcc<-df2genind(compdiv[,11:24],ncode=6,pop=compdiv$host,ploidy=2,
 
 #Allelic richness for the full temporal dataset
 Host_Ar<-apply(AllRich(COMPDI)[[2]],1,mean)
-#we set the minimum number of samples to 5 (the minimum in the 
+#we set the minimum number of samples to 16 (the minimum in the 
 #clone-corrected dataset)
 Host_Ar<-apply(AllRich(COMPDI,16)[[2]],1,mean)
 #Allelic richness for the clone-corrected temporal dataset
@@ -54,7 +55,7 @@ Host_Arcc<-apply(AllRich(COMPDIcc)[[2]],1,mean)
 
 #Private Allelic richness for the full temporal dataset
 Host_PrivAr<-apply(PrivAllRich(COMPDI)[[2]],1,mean)
-#we set the minimum number of samples to 5 (the minimum in the 
+#we set the minimum number of samples to 16 (the minimum in the 
 #clone-corrected dataset)
 Host_PrivAr<-apply(PrivAllRich(COMPDI,16)[[2]],1,mean)
 #Private Allelic richness for the clone-corrected temporal dataset
