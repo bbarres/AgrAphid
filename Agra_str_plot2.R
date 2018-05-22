12fb16eedd68e95933e1be94ccd23e3c6ea7c240
bbarres <benoit.barres@gmail.com>
2018-05-22 18:39:37 +0200

    new structure plot taking advantage of the CLUMPAK output for K=2 to 6

--- Agra_str_plot2.R
+++ Agra_str_plot2.R
@@ -25,21 +25,33 @@ setwd("~/work/Rfichiers/Githuber/AgrAphid_data")
 #We first need to edit a little the output file in excel prior to the 
 #importation: just keep the q matrix without any other information
 
-#for K=2
-K2_100runs<-read.table("AgrAccconsK2.perm_datafile",header=FALSE,
+#for K=2, import the 100 run datafile (100 q matrix)
+K2_100runs<-read.table("AgrAccconsK2.ind_datafile",header=FALSE,
                        blank.lines.skip=TRUE,sep="\t")
+#import the column order for the best CLUMPP "permutation"
+K2_colorder<-read.table("AgrAccconsK2.ind_colorder",header=FALSE,
+                        blank.lines.skip=TRUE,sep="\t")
 #then we split the dataframe in as many repetition that has been made
 #by the number of individuals (here 309)
 K2_100runs<-split(K2_100runs,rep(1:100,each=309))
-K2_100runs[[1]]
-coloor <- c("royalblue4","firebrick","chartreuse4","khaki2","darkorange")
+#reordering the columns so that the different repetition colorization fit
+for (i in 1:100){
+  K2_100runs[[i]]<-K2_100runs[[i]][as.numeric(K2_colorder[i,])]
+}
+#importing the order of the run so that the different repetition corresponding
+#to the same clustering solution followed each other
+K2_reporder<-read.table("AgrAccconsK2.ind_linorder",header=FALSE,
+                        blank.lines.skip=TRUE,sep="\t")+1
+
+coloor <- c("chartreuse4","firebrick","khaki2","darkorange","royalblue4")
 effpop<-c(69,29,11,16,168,16)
 poptiquet<-c("Peach","Oilseed rape","Tobacco","Other Crops","Aerial Trap",
              "Multiple hosts")
 #now we can plot the 100 runs on the same figure
 op<-par(mfrow=c(100,1),mar=c(0,0,0,0),oma=c(1,0,3,0))
 for (i in 1:100){
-  temp<-K2_100runs[[i]]
+  j<-as.numeric(K2_reporder[i])
+  temp<-K2_100runs[[j]]
   structplot(t(temp),coloor,effpop,poptiquet,spacepop=2,
              leg_y="K=2",cexy=1.2,mef=c(0,0,0,0,0),colbord=NA,
              distxax=0.15,angl=0,cexpop=1.5)
@@ -48,20 +60,33 @@ title(main="K=2",cex.main=2.5,outer=TRUE)
 par(op)
 #export pdf 25 x 12
 
-#for K=3
-K3_100runs<-read.table("AgrAccconsK3.perm_datafile",header=FALSE,
+#for K=3, import the 100 run datafile (100 q matrix)
+K3_100runs<-read.table("AgrAccconsK3.ind_datafile",header=FALSE,
                        blank.lines.skip=TRUE,sep="\t")
+#import the column order for the best CLUMPP "permutation"
+K3_colorder<-read.table("AgrAccconsK3.ind_colorder",header=FALSE,
+                        blank.lines.skip=TRUE,sep="\t")
 #then we split the dataframe in as many repetition that has been made
 #by the number of individuals (here 309)
 K3_100runs<-split(K3_100runs,rep(1:100,each=309))
-coloor <- c("royalblue4","chartreuse4","firebrick","khaki2","darkorange")
+#reordering the columns so that the different repetition colorization fit
+for (i in 1:100){
+  K3_100runs[[i]]<-K3_100runs[[i]][as.numeric(K3_colorder[i,])]
+}
+#importing the order of the run so that the different repetition corresponding
+#to the same clustering solution followed each other
+K3_reporder<-read.table("AgrAccconsK3.ind_linorder",header=FALSE,
+                        blank.lines.skip=TRUE,sep="\t")+1
+
+coloor <- c("chartreuse4","firebrick","khaki2","darkorange","royalblue4")
 effpop<-c(69,29,11,16,168,16)
 poptiquet<-c("Peach","Oilseed rape","Tobacco","Other Crops","Aerial Trap",
              "Multiple hosts")
 #now we can plot the 100 runs on the same figure
 op<-par(mfrow=c(100,1),mar=c(0,0,0,0),oma=c(1,0,3,0))
 for (i in 1:100){
-  temp<-K3_100runs[[i]]
+  j<-as.numeric(K3_reporder[i])
+  temp<-K3_100runs[[j]]
   structplot(t(temp),coloor,effpop,poptiquet,spacepop=2,
              leg_y="K=3",cexy=1.2,mef=c(0,0,0,0,0),colbord=NA,
              distxax=0.15,angl=0,cexpop=1.5)
@@ -70,20 +95,33 @@ title(main="K=3",cex.main=2.5,outer=TRUE)
 par(op)
 #export pdf 25 x 12
 
-#for K=4
-K4_100runs<-read.table("AgrAccconsK4.perm_datafile",header=FALSE,
+#for K=4, import the 100 run datafile (100 q matrix)
+K4_100runs<-read.table("AgrAccconsK4.ind_datafile",header=FALSE,
                        blank.lines.skip=TRUE,sep="\t")
+#import the column order for the best CLUMPP "permutation"
+K4_colorder<-read.table("AgrAccconsK4.ind_colorder",header=FALSE,
+                        blank.lines.skip=TRUE,sep="\t")
 #then we split the dataframe in as many repetition that has been made
 #by the number of individuals (here 309)
 K4_100runs<-split(K4_100runs,rep(1:100,each=309))
-coloor <- c("khaki2","royalblue4","chartreuse4","firebrick","darkorange")
+#reordering the columns so that the different repetition colorization fit
+for (i in 1:100){
+  K4_100runs[[i]]<-K4_100runs[[i]][as.numeric(K4_colorder[i,])]
+}
+#importing the order of the run so that the different repetition corresponding
+#to the same clustering solution followed each other
+K4_reporder<-read.table("AgrAccconsK4.ind_linorder",header=FALSE,
+                        blank.lines.skip=TRUE,sep="\t")+1
+
+coloor <- c("chartreuse4","firebrick","khaki2","darkorange","royalblue4")
 effpop<-c(69,29,11,16,168,16)
 poptiquet<-c("Peach","Oilseed rape","Tobacco","Other Crops","Aerial Trap",
              "Multiple hosts")
 #now we can plot the 100 runs on the same figure
 op<-par(mfrow=c(100,1),mar=c(0,0,0,0),oma=c(1,0,3,0))
 for (i in 1:100){
-  temp<-K4_100runs[[i]]
+  j<-as.numeric(K4_reporder[i])
+  temp<-K4_100runs[[j]]
   structplot(t(temp),coloor,effpop,poptiquet,spacepop=2,
              leg_y="K=4",cexy=1.2,mef=c(0,0,0,0,0),colbord=NA,
              distxax=0.15,angl=0,cexpop=1.5)
@@ -127,21 +165,34 @@ title(main="K=5",cex.main=2.5,outer=TRUE)
 par(op)
 #export pdf 25 x 12
 
-#for K=6
-K6_100runs<-read.table("AgrAccconsK6.perm_datafile",header=FALSE,
+#for K=6, import the 100 run datafile (100 q matrix)
+K6_100runs<-read.table("AgrAccconsK6.ind_datafile",header=FALSE,
                        blank.lines.skip=TRUE,sep="\t")
+#import the column order for the best CLUMPP "permutation"
+K6_colorder<-read.table("AgrAccconsK6.ind_colorder",header=FALSE,
+                        blank.lines.skip=TRUE,sep="\t")
 #then we split the dataframe in as many repetition that has been made
 #by the number of individuals (here 309)
 K6_100runs<-split(K6_100runs,rep(1:100,each=309))
-coloor <- c("chartreuse4","royalblue4","firebrick","grey20","khaki2",
-            "darkorange")
+#reordering the columns so that the different repetition colorization fit
+for (i in 1:100){
+  K6_100runs[[i]]<-K6_100runs[[i]][as.numeric(K6_colorder[i,])]
+}
+#importing the order of the run so that the different repetition corresponding
+#to the same clustering solution followed each other
+K6_reporder<-read.table("AgrAccconsK6.ind_linorder",header=FALSE,
+                        blank.lines.skip=TRUE,sep="\t")+1
+
+coloor <- c("chartreuse4","firebrick","khaki2","darkorange","royalblue4",
+            "grey30")
 effpop<-c(69,29,11,16,168,16)
 poptiquet<-c("Peach","Oilseed rape","Tobacco","Other Crops","Aerial Trap",
              "Multiple hosts")
 #now we can plot the 100 runs on the same figure
 op<-par(mfrow=c(100,1),mar=c(0,0,0,0),oma=c(1,0,3,0))
 for (i in 1:100){
-  temp<-K6_100runs[[i]]
+  j<-as.numeric(K6_reporder[i])
+  temp<-K6_100runs[[j]]
   structplot(t(temp),coloor,effpop,poptiquet,spacepop=2,
              leg_y="K=6",cexy=1.2,mef=c(0,0,0,0,0),colbord=NA,
              distxax=0.15,angl=0,cexpop=1.5)
