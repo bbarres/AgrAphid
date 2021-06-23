0ee6bb6dc748228e25075ef924c90803f2d48d73
bbarres <benoit.barres@gmail.com>
2021-06-24 00:31:37 +0200

    polishing the 100 STRUCUTRE runs supplementary figures

--- Agra_str_plot2.R
+++ Agra_str_plot2.R
@@ -37,22 +37,27 @@ for (i in 1:100){
 K2_reporder<-read.table("data/AgrAccconsK2.ind_linorder",header=FALSE,
                         blank.lines.skip=TRUE,sep="\t")+1
 
-coloor <- c("chartreuse4","firebrick","khaki2","darkorange","royalblue4")
+coloor<-c("chartreuse4","firebrick","khaki2","darkorange","royalblue4")
 effpop<-c(69,29,11,16,168,16)
 poptiquet<-c("Peach","Oilseed rape","Tobacco","Other Crops","Aerial Trap",
              "Multiple hosts")
 #now we can plot the 100 runs on the same figure
-op<-par(mfrow=c(100,1),mar=c(0,0,0,0),oma=c(1,0,3,0))
-for (i in 1:100){
+op<-par(mfrow=c(100,1),mar=c(0.2,0,0,0),oma=c(9,0,3,0))
+for (i in 1:99){
   j<-as.numeric(K2_reporder[i])
   temp<-K2_100runs[[j]]
   structplot(t(temp),coloor[c(2,1)],effpop,poptiquet,spacepop=2,
-             leg_y="K=2",cexy=1.2,mef=c(0,0,0,0,0),colbord=NA,
+             leg_y="K=2",cexy=1.2,mef=c(0,1,0,0,0),colbord=NA,
              distxax=0.15,angl=0,cexpop=1.5)
 }
+j<-as.numeric(K2_reporder[100])
+temp<-K2_100runs[[j]]
+structplot(t(temp),coloor[c(2,1)],effpop,poptiquet,spacepop=2,
+           leg_y="K=2",cexy=1.2,mef=c(0,1,1,1,0),colbord=NA,
+           distxax=1,angl=20,cexpop=2.5)
 title(main="K=2",cex.main=2.5,outer=TRUE)
 par(op)
-#export pdf 21 x 29.7
+#export pdf 15 x 30
 
 #for K=3, import the 100 run datafile (100 q matrix)
 K3_100runs<-read.table("data/AgrAccconsK3.ind_datafile",header=FALSE,
@@ -77,17 +82,22 @@ effpop<-c(69,29,11,16,168,16)
 poptiquet<-c("Peach","Oilseed rape","Tobacco","Other Crops","Aerial Trap",
              "Multiple hosts")
 #now we can plot the 100 runs on the same figure
-op<-par(mfrow=c(100,1),mar=c(0,0,0,0),oma=c(1,0,3,0))
-for (i in 1:100){
+op<-par(mfrow=c(100,1),mar=c(0.2,0,0,0),oma=c(9,0,3,0))
+for (i in 1:99){
   j<-as.numeric(K3_reporder[i])
   temp<-K3_100runs[[j]]
   structplot(t(temp),coloor[c(2,1,3)],effpop,poptiquet,spacepop=2,
-             leg_y="K=3",cexy=1.2,mef=c(0,0,0,0,0),colbord=NA,
+             leg_y="K=3",cexy=1.2,mef=c(0,1,0,0,0),colbord=NA,
              distxax=0.15,angl=0,cexpop=1.5)
 }
+j<-as.numeric(K3_reporder[100])
+temp<-K3_100runs[[j]]
+structplot(t(temp),coloor[c(2,1,3)],effpop,poptiquet,spacepop=2,
+           leg_y="K=3",cexy=1.2,mef=c(0,1,1,1,0),colbord=NA,
+           distxax=1,angl=20,cexpop=2.5)
 title(main="K=3",cex.main=2.5,outer=TRUE)
 par(op)
-#export pdf 21 x 29.7
+#export pdf 15 x 30
 
 #for K=4, import the 100 run datafile (100 q matrix)
 K4_100runs<-read.table("data/AgrAccconsK4.ind_datafile",header=FALSE,
@@ -112,17 +122,22 @@ effpop<-c(69,29,11,16,168,16)
 poptiquet<-c("Peach","Oilseed rape","Tobacco","Other Crops","Aerial Trap",
              "Multiple hosts")
 #now we can plot the 100 runs on the same figure
-op<-par(mfrow=c(100,1),mar=c(0,0,0,0),oma=c(1,0,3,0))
-for (i in 1:100){
+op<-par(mfrow=c(100,1),mar=c(0.2,0,0,0),oma=c(9,0,3,0))
+for (i in 1:99){
   j<-as.numeric(K4_reporder[i])
   temp<-K4_100runs[[j]]
   structplot(t(temp),coloor[c(1,2,4,3)],effpop,poptiquet,spacepop=2,
-             leg_y="K=4",cexy=1.2,mef=c(0,0,0,0,0),colbord=NA,
+             leg_y="K=4",cexy=1.2,mef=c(0,1,0,0,0),colbord=NA,
              distxax=0.15,angl=0,cexpop=1.5)
 }
+j<-as.numeric(K4_reporder[100])
+temp<-K4_100runs[[j]]
+structplot(t(temp),coloor[c(1,2,4,3)],effpop,poptiquet,spacepop=2,
+           leg_y="K=4",cexy=1.2,mef=c(0,1,1,1,0),colbord=NA,
+           distxax=1,angl=20,cexpop=2.5)
 title(main="K=4",cex.main=2.5,outer=TRUE)
 par(op)
-#export pdf 21 x 29.7
+#export pdf 15 x 30
 
 #for K=5, import the 100 run datafile (100 q matrix)
 K5_100runs<-read.table("data/AgrAccconsK5.ind_datafile",header=FALSE,
@@ -147,17 +162,22 @@ effpop<-c(69,29,11,16,168,16)
 poptiquet<-c("Peach","Oilseed rape","Tobacco","Other Crops","Aerial Trap",
              "Multiple hosts")
 #now we can plot the 100 runs on the same figure
-op<-par(mfrow=c(100,1),mar=c(0,0,0,0),oma=c(1,0,3,0))
-for (i in 1:100){
+op<-par(mfrow=c(100,1),mar=c(0.2,0,0,0),oma=c(9,0,3,0))
+for (i in 1:99){
   j<-as.numeric(K5_reporder[i])
   temp<-K5_100runs[[j]]
   structplot(t(temp),coloor[c(1,2,3,5,4)],effpop,poptiquet,spacepop=2,
-             leg_y="K=5",cexy=1.2,mef=c(0,0,0,0,0),colbord=NA,
+             leg_y="K=5",cexy=1.2,mef=c(0,1,0,0,0),colbord=NA,
              distxax=0.15,angl=0,cexpop=1.5)
 }
+j<-as.numeric(K5_reporder[100])
+temp<-K5_100runs[[j]]
+structplot(t(temp),coloor[c(1,2,3,5,4)],effpop,poptiquet,spacepop=2,
+           leg_y="K=5",cexy=1.2,mef=c(0,1,1,1,0),colbord=NA,
+           distxax=1,angl=20,cexpop=2.5)
 title(main="K=5",cex.main=2.5,outer=TRUE)
 par(op)
-#export pdf 21 x 29.7
+#export pdf 15 x 30
 
 #for K=6, import the 100 run datafile (100 q matrix)
 K6_100runs<-read.table("data/AgrAccconsK6.ind_datafile",header=FALSE,
@@ -178,19 +198,24 @@ K6_reporder<-read.table("data/AgrAccconsK6.ind_linorder",header=FALSE,
                         blank.lines.skip=TRUE,sep="\t")+1
 
 coloor <- c("chartreuse4","firebrick","khaki2","darkorange","royalblue4",
-            "grey30")
+            "grey50")
 effpop<-c(69,29,11,16,168,16)
 poptiquet<-c("Peach","Oilseed rape","Tobacco","Other Crops","Aerial Trap",
              "Multiple hosts")
 #now we can plot the 100 runs on the same figure
-op<-par(mfrow=c(100,1),mar=c(0,0,0,0),oma=c(1,0,3,0))
-for (i in 1:100){
+op<-par(mfrow=c(100,1),mar=c(0.2,0,0,0),oma=c(9,0,3,0))
+for (i in 1:99){
   j<-as.numeric(K6_reporder[i])
   temp<-K6_100runs[[j]]
   structplot(t(temp),coloor[c(5,2,3,1,4,6)],effpop,poptiquet,spacepop=2,
-             leg_y="K=6",cexy=1.2,mef=c(0,0,0,0,0),colbord=NA,
+             leg_y="K=6",cexy=1.2,mef=c(0,1,0,0,0),colbord=NA,
              distxax=0.15,angl=0,cexpop=1.5)
 }
+j<-as.numeric(K6_reporder[100])
+temp<-K6_100runs[[j]]
+structplot(t(temp),coloor[c(5,2,3,1,4,6)],effpop,poptiquet,spacepop=2,
+           leg_y="K=6",cexy=1.2,mef=c(0,1,1,1,0),colbord=NA,
+           distxax=1,angl=20,cexpop=2.5)
 title(main="K=6",cex.main=2.5,outer=TRUE)
 par(op)
 #export pdf 25 x 12
