cb2ebfc8d975591e58d0d32529144937bc83c611
bbarres <benoit.barres@gmail.com>
2021-07-13 12:43:31 +0200

    addition of the diversity indices for each host

--- Agra_host_div.R
+++ Agra_host_div.R
@@ -8,19 +8,65 @@ source("Agra_load.R")
 
 
 ##############################################################################/
-#MLG Diversity indices by genetic cluster####
+#MLG Diversity indices by host population####
 ##############################################################################/
 
 #To compute the diversity we need a presence/absence of MLG table
-#for each genetic cluster
-Aerial_cor<-Aerial[!is.na(Aerial$Clust_K4),]
-hostocc<-table(Aerial_cor$Clust_K4,Aerial_cor$MLG_ID)
+#for each host population
+datHost<-datAgra[datAgra$host!="Aerial_trap",]
+datHost<-drop.levels(datHost)
+hostocc<-table(datHost$host,datHost$MLG_ID)
 Host_nb_samples<-rowSums(hostocc)
 Host_nb_MLG<-specnumber(hostocc)
 Host_GsurN<-specnumber(hostocc)/rowSums(hostocc)
 Host_MLG_richness<-rarefy(hostocc,min(rowSums(hostocc)))
 Host_simpson_div<-vegan::diversity(hostocc,index="simpson")
 Host_pielou_even<-vegan::diversity(hostocc)/log(specnumber(hostocc))
+
+
+##############################################################################/
+#Genetic Diversity indices by Cluster on the complete dataset####
+##############################################################################/
+
+#converting data to a genind format for the full dataset
+compdiv<-datHost #name of the input file
+COMPDI<-df2genind(compdiv[,11:24],ncode=3,pop=compdiv$host,ploidy=2,
+                  NA.char=c("999"),ind.names=as.character(compdiv$indiv_ID))
+#Allelic richness for the full temporal dataset
+Host_Ar<-apply(AllRich(COMPDI)[[2]],1,mean)
+#Private Allelic richness for the full temporal dataset
+Host_PrivAr<-apply(PrivAllRich(COMPDI)[[2]],1,mean)
+#Nei heterozygosity for the full temporal dataset
+Host_HetNei<-Hs(COMPDI)
+
+
+##############################################################################/
+#building the final table for host population diversity####
+##############################################################################/
+
+divHosttable<-cbind(Host_nb_samples,Host_nb_MLG,Host_GsurN,Host_MLG_richness,
+                   Host_simpson_div,Host_pielou_even,Host_Ar,Host_PrivAr,
+                   Host_HetNei)
+write.table(divHosttable,file="output/divHosttable.txt",
+            quote=FALSE,sep="\t")
+
+
+
+
+##############################################################################/
+#MLG Diversity indices by genetic cluster####
+##############################################################################/
+
+#To compute the diversity we need a presence/absence of MLG table
+#for each genetic cluster
+Aerial_cor<-Aerial[!is.na(Aerial$Clust_K4),]
+clustocc<-table(Aerial_cor$Clust_K4,Aerial_cor$MLG_ID)
+Clust_nb_samples<-rowSums(clustocc)
+Clust_nb_MLG<-specnumber(clustocc)
+Clust_GsurN<-specnumber(clustocc)/rowSums(clustocc)
+Clust_MLG_richness<-rarefy(clustocc,min(rowSums(clustocc)))
+Clust_simpson_div<-vegan::diversity(clustocc,index="simpson")
+Clust_pielou_even<-vegan::diversity(clustocc)/log(specnumber(clustocc))
 
 
 ##############################################################################/
@@ -38,21 +84,22 @@ COMPDIcc<-df2genind(compdiv[,11:24],ncode=3,pop=compdiv$Clust_K4,ploidy=2,
                     NA.char=c("999"))
 
 #Allelic richness for the full temporal dataset
-Host_Ar<-apply(AllRich(COMPDI)[[2]],1,mean)
+Clust_Ar<-apply(AllRich(COMPDI)[[2]],1,mean)
 #Private Allelic richness for the full temporal dataset
-Host_PrivAr<-apply(PrivAllRich(COMPDI)[[2]],1,mean)
+Clust_PrivAr<-apply(PrivAllRich(COMPDI)[[2]],1,mean)
 #Nei heterozygosity for the full temporal dataset
-Host_HetNei<-Hs(COMPDI)
+Clust_HetNei<-Hs(COMPDI)
 
 
 ##############################################################################/
-#building the final table####
+#building the final table for genetic cluster diversity####
 ##############################################################################/
 
-divtable<-cbind(Host_nb_samples,Host_nb_MLG,Host_GsurN,Host_MLG_richness,
-                Host_simpson_div,Host_pielou_even,Host_Ar,Host_PrivAr,
-                Host_HetNei)
-write.table(divtable,file="output/divtable.txt",quote=FALSE,sep="\t")
+divClusttable<-cbind(Clust_nb_samples,Clust_nb_MLG,Clust_GsurN,
+                     Clust_MLG_richness,Clust_simpson_div,Clust_pielou_even,
+                     Clust_Ar,Clust_PrivAr,Clust_HetNei)
+write.table(divClusttable,file="output/divClusttable.txt",
+            quote=FALSE,sep="\t")
 
 
 ##############################################################################/
