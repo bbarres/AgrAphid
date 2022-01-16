14c25e734b4fa130e67c81a0b494491ca1dcb955
bbarres <benoit.barres@gmail.com>
2022-01-17 00:42:18 +0100

    adding LD computation between microsatellites and resistance markers

--- Agra_genepop_ana.R
+++ Agra_genepop_ana.R
@@ -388,5 +388,64 @@ upViewport()
 
 
 ##############################################################################/
+#Linkage disequilibrium in Host populations including resistance markers####
+##############################################################################/
+
+onebyhost<-"data/onebyhostALL.txt"
+test_LD(onebyhost,"output/onebyhostALL.txt.DIS")
+clean_workdir()
+
+#the output file have to be edited so it can be used for representation
+LDbyhoAL<-readLines("output/onebyhostALL.txt.DIS")
+#removing the first lines that are non relevant
+LDcolnames<-unlist(strsplit(LDbyhoAL[13],"\\s+"))
+LDbyhoAL<-LDbyhoAL[-c(1:14)]
+#for 14 markers and 4 populations there are 
+18*17/2*4 #LD tests for markers pairs
+LDbyhoAL<-LDbyhoAL[-c(613:length(LDbyhoAL))]
+LDbyhoAL<-as.data.table(matrix(unlist(strsplit(LDbyhoAL,"\\s+")),
+                               nrow=length(strsplit(LDbyhoAL,"\\s+")),
+                               byrow=TRUE))
+colnames(LDbyhoAL)<-LDcolnames
+LDbyhoAL$`P-Value`<-as.numeric(LDbyhoAL$`P-Value`)
+
+#Linkage disequilibrium in the peach population
+LDpeach<-as.matrix(LDbyhoAL[c(1:153),])
+row.names(LDpeach)<-paste(LDpeach[,2],LDpeach[,3])
+LDpeach<-rbind(LDpeach[,c(2,3,4)],LDpeach[,c(3,2,4)])
+LDpeach<-spread(data.frame(LDpeach),2,3,fill="NA",convert=TRUE)
+#changing the rownames
+row.names(LDpeach)<-as.character(LDpeach[,1])
+#removing the first unnecessary column
+LDpeach<-LDpeach[,c(-1)]
+#reordering the columns and turning the object into a matrix
+LDpeach<-as.matrix(LDpeach[c(1,18,2,17,4,10,14,15,16,3,5:9,11:13),
+                           c(1,18,2,17,4,10,14,15,16,3,5:9,11:13)])
+
+#peach
+temp<-LDpeach
+#scaling the p-value so it is easily usable with the LDheatmap function
+temp[temp>0.5]<-0.9
+temp[temp>0.10 & temp<0.9]<-0.8
+temp[temp>0.00055 & temp<0.8]<-0.6
+temp[temp>0.00011 & temp<0.6]<-0.4
+temp[temp>0.000011 & temp<0.4]<-0.2
+temp[temp<0.001]<-0.1
+#the actual plotting start here
+PEACH<-LDheatmap(temp,title=NULL,
+                 add.map=FALSE,distances=NULL,SNP.name=row.names(temp),
+                 color=c(rep(grey(0.8),3),
+                         brewer.pal(6,"YlOrRd")[c(2,4,6)]),
+                 name="peach",flip=TRUE,add.key=FALSE,
+                 newpage=FALSE)
+grid.edit(gPath("peach","heatMap","heatmap"),gp=gpar(col="white",lwd=2.5))
+grid.edit(gPath("peach","SNPnames"),
+          gp=gpar(col="black",rot="0",cex=1.2,font=2),
+          rot=0,hjust=0.8)
+grid.text("Peach",x=unit(0.5,"npc"),y=unit(0.75,"npc"),
+          gp=gpar(fontsize=40),check=TRUE)
+
+
+##############################################################################/
 #END
 ##############################################################################/
